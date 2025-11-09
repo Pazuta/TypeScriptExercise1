503088db876bc944efdcbb6bb7fceef918112520
Pazuta <t.beier233@gmail.com>
2025-11-09 21:27:18 +0100

    implement mensaCounter function for Leipzig

--- test.ts
+++ test.ts
@@ -1,7 +1,21 @@
 // Mensa URL: https://openmensa.org/api/v2/canteens/
 
 const mensaUrl: string = "https://openmensa.org/api/v2/canteens/" // "https://bored-api.appbrewery.com/random"
+const city: string = "Leipzig"
 
-let myResponse: Response = await fetch(mensaUrl) 
+async function mensaCounter(url: string, city: string) {
 
-console.log(await myResponse.json())
+    let myResponse: Response = await fetch(mensaUrl);
+    let data = await myResponse.json();
+
+    // Filtert nur die Einträge mit city === "Leipzig"
+    let leipzigMensen = data.filter((mensa: any) => mensa.city === "Leipzig");
+
+    // Anzahl der Leipziger Mensen
+    let anzahlCity = leipzigMensen.length;
+
+    console.log(leipzigMensen);
+    console.log("Anzahl Mensen in " + city + ": " + anzahlCity);
+}
+
+mensaCounter(mensaUrl, city)
