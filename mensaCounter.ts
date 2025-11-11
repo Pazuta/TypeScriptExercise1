72581a433937600fac6aa47db71bf0d9b6412d57
Pazuta <156230695+Pazuta@users.noreply.github.com>
2025-11-11 12:40:43 +0100

    split mensaCounter into subfunctions

--- mensaCounter.ts
+++ mensaCounter.ts
@@ -3,38 +3,43 @@
 const mensaUrl: string = "https://openmensa.org/api/v2/canteens/" // "https://bored-api.appbrewery.com/random"
 const city: string = "Leipzig"
 
-async function mensaCounter(url: string, city: string) {
+async function openMensaAPI(url: string, city: string): Promise<string[]> {            // ruft OpenMensaAPI auf
 
-    let myResponse: Response = await fetch(mensaUrl);
-    let data = await myResponse.json();
-
+    const myResponse: Response = await fetch(mensaUrl);
+    const data = await myResponse.json();
     // Filtert nur die Einträge der angegebenen Stadt
-    let mensenOfCity = data.filter((mensa: any) => mensa.city === city);
+    const mensenInCity: string[] = data.filter((mensa: any) => mensa.city === city);
+    // console.log(await mensenInCity)                     // Array wird in der Konsole ausgegeben
+    return mensenInCity;                            // bei "console.log(await openMensaAPI(mensaUrl, city))"  wird Promise zurückgegeben
 
+}    
+
+function createMensaNameArray(mensenInCity: any): string[] {                  // speichert Namen der Mensen in einem Array
     
-
-    // Gibt nur die Namen der Mensen aus
-    const names: string[] = [];
-
-    mensenOfCity.forEach((mensa: any) => {
+    let names: string[] = [];
+    console.log(mensenInCity);
+    mensenInCity.forEach((mensa: any) => {
         names.push(mensa.name);
     });
+    return names;
+}
 
+function countedMensaInCity(mensenInCity: any, names: string[]) {   // zählt Mensen in der Stadt
 
-    // Anzahl der Leipziger Mensen
-    let anzahlCity = mensenOfCity.length;
-
+    let countedMensa = mensenInCity.length;
     console.log(names);
-    console.log("Anzahl Mensen in " + city + ": " + anzahlCity);
+    console.log("Anzahl Mensen in " + city + ": " + countedMensa);
+    return countedMensa;
 
-    // HTML Anzeige
+}
 
+function buildHTML(countedMensa: any, names: string[]) {              // baut HTML Seite auf
     
     let container = document.getElementById('container');
     if (container) {
 
         let element = document.createElement('div');
-        element.innerText = "Anzahl Mensen in " + city + ": " + anzahlCity;
+        element.innerText = "Anzahl Mensen in " + city + ": " + countedMensa;
         container.appendChild(element);
         
         for (let i = 0; i < names.length; i++) {
@@ -46,7 +51,28 @@ async function mensaCounter(url: string, city: string) {
     } else {
         console.error('Container element with id "container" not found');
     }
-    
 }
 
-mensaCounter(mensaUrl, city)
+function main() {
+
+    let mensenData: string[] = [];
+    openMensaAPI(mensaUrl, city).then(mensenInCity => {
+        mensenData = mensenInCity;
+        let names: string[] = createMensaNameArray(mensenData);
+        var countedMensa = countedMensaInCity(mensenData, names);
+        buildHTML(countedMensa, names);
+    });
+
+}
+
+main();
+
+
+/*
+openMensaAPI(mensaUrl, city).then(mensenInCity => {
+    console.log(mensenInCity)
+});
+
+ main();
+
+*/
